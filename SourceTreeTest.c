eca7e11806adae6e6f349f37726fba4d1c3a4c34
Syntia Listio <tia.listio@gmail.com>
2018-05-02 10:57:33 +0900

    Test 브랜치에 코드를 추가함

--- SourceTreeTest.c
+++ SourceTreeTest.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
 int int main() {
+  int i = 0;
 
   printf("First OSS Code\n");
   printf("Third OSS Code\n");
 
+  for(int i =0 ; i < 10 ; i++){
+    print("Test 브랱치\n")
+  }
   return 0;
 }
